3dd0dab84af3a9d23ce332a03a7b8b2e10065dc0
mendac01 <mendac01@pfw.edu>
2020-11-03 16:19:36 -0500

    added functionality to AcronymGenerator

--- notes/week_1/day_2/AcronymGenerator/Program.cs
+++ notes/week_1/day_2/AcronymGenerator/Program.cs
@@ -6,7 +6,18 @@ namespace AcronymGenerator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a series of words separated by spaces:");
+            string line = Console.ReadLine();
+            string[] words = line.Split(" ");
+            
+            string acronym ="";
 
+            for (int i = 0; i < words.Length; i++)
+            {
+                acronym += words[i][0];
+            }
+
+            Console.WriteLine(acronym.ToUpper());
         }
     }
 }
