9512064bdb44a8ad0a750b4a90e3be91d8dc1886
mendac01 <mendac01@pfw.edu>
2020-11-03 12:53:40 -0500

    adding notes and practice files

---
+++ notes/week_1/day_2/numbers-quickstart/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumbersInSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculateAreaOfCircle(2.5));
+        }
+
+        static double CalculateAreaOfCircle(double r)
+        {
+            return Math.PI*(r*r);
+        }
+    }
+}
