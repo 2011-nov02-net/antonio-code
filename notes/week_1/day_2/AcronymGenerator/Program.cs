df77bcc76fc745d32d5e3be1aaf3b9efcaebac1a
mendac01 <mendac01@pfw.edu>
2020-11-03 16:09:15 -0500

    init AcronymGenerator

---
+++ notes/week_1/day_2/AcronymGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AcronymGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ notes/week_1/day_2/ProceduralBasics/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ProceduralBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
